3a4e99a2ac458878dfad4a4130a4e93a8fba7bc0
Aly <alyzopairaly@gmail.com>
2026-02-26 07:14:19 +0200

    Global Intel Implementation: app/src/main/java/com/clt/sovereign/MainActivity.kt

--- app/src/main/java/com/clt/sovereign/MainActivity.kt
+++ app/src/main/java/com/clt/sovereign/MainActivity.kt
@@ -4,15 +4,17 @@ package com.clt.sovereign
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import android.graphics.Color
 import android.view.Gravity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val textView = TextView(this)
-        textView.text = "CLT Sovereign System Initialized"
-        textView.textSize = 24f
-        textView.gravity = Gravity.CENTER
-        setContentView(textView)
+        val tv = TextView(this)
+        tv.text = "CLT SOVEREIGN SYSTEM\nBUILD #34 SUCCESS"
+        tv.setTextColor(Color.GREEN)
+        tv.textSize = 30f
+        tv.gravity = Gravity.CENTER
+        setContentView(tv)
     }
 }
