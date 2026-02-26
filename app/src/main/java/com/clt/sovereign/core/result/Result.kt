9ff67b1c9c714a8b89d1ff6741f3ae32715bf7f0
Aly <alyzopairaly@gmail.com>
2026-02-26 23:22:49 +0200

    Nuclear Format: Cleaning for Sovereign Restart

--- app/src/main/java/com/clt/sovereign/core/result/Result.kt
+++
@@ -1,5 +0,0 @@
-package com.clt.sovereign.core.result
-sealed class Result<out T> {
-    data class Success<T>(val data: T): Result<T>()
-    data class Failure(val message: String): Result<Nothing>()
-}
