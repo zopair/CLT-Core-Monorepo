14b4e62fba50120fe57943e0e1446016bf586276
Aly <alyzopairaly@gmail.com>
2026-02-26 05:06:11 +0200

    Setup app/src/main/java/com/clt/sovereign/CoreActivity.kt

---
+++ app/src/main/java/com/clt/sovereign/CoreActivity.kt
@@ -0,0 +1,5 @@
+package com.clt.sovereign
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class CoreActivity : ComponentActivity() { override fun onCreate(s: Bundle?) { super.onCreate(s) } }
